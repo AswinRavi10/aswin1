56dc7f837133baef5a4dc2c930d15b1ed1adb728
savan fami <savanfami1239@gmail.com>
2023-03-17 12:34:01 +0530

    Initial commit

---
+++ bin/aswin1.dart
@@ -0,0 +1,5 @@
+import 'package:aswin1/aswin1.dart' as aswin1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aswin1.calculate()}!');
+}
---
+++ bin/sample1.dart
@@ -0,0 +1,12 @@
+//dfjh
+//void main(){
+//  print('hello');
+//}
+// void main(){
+//   print('aswin');
+//   print(10);
+// }
+void main(){
+  print(10);
+  print('aswin');
+}
